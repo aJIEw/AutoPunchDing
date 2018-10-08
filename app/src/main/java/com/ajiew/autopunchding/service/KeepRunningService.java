6aaa0d772a88a04184fce6a9115289ce32634ebd
aJIEw <kevin10tod@gmail.com>
2018-10-08 09:43:09 +0800

    fix notification issue for Android O and above

--- app/src/main/java/com/ajiew/autopunchding/service/KeepRunningService.java
+++ app/src/main/java/com/ajiew/autopunchding/service/KeepRunningService.java
@@ -2,16 +2,21 @@ package com.ajiew.autopunchding.service;
 
 import android.app.AlarmManager;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 
-import com.ajiew.autopunchding.broadcast.AutoStartReceiver;
 import com.ajiew.autopunchding.R;
+import com.ajiew.autopunchding.broadcast.AutoStartReceiver;
 import com.ajiew.autopunchding.broadcast.PunchReceiver;
 
 /**
@@ -19,7 +24,7 @@ import com.ajiew.autopunchding.broadcast.PunchReceiver;
  * created on: 2018/9/9 15:21
  * description: Make sure this will keep running
  */
-public class KeepRunningService extends Service{
+public class KeepRunningService extends Service {
 
     private PunchReceiver receiver;
 
@@ -44,6 +49,25 @@ public class KeepRunningService extends Service{
                 .setContentText("Day Day Up!")
                 .setSmallIcon(R.mipmap.ic_launcher_round)
                 .setWhen(System.currentTimeMillis());
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String NOTIFICATION_CHANNEL_ID = this.getClass().getCanonicalName();
+            String channelName = this.getClass().getSimpleName();
+            NotificationChannel channel = new NotificationChannel(
+                    NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+            channel.setLightColor(Color.BLUE);
+            channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            assert manager != null;
+            manager.createNotificationChannel(channel);
+
+            builder = new Notification.Builder(this.getApplicationContext(), NOTIFICATION_CHANNEL_ID)
+                    .setContentTitle("AutoPunchDing")
+                    .setContentText("Day Day Up!")
+                    .setSmallIcon(R.mipmap.ic_launcher_round)
+                    .setWhen(System.currentTimeMillis())
+                    .setCategory(Notification.CATEGORY_SERVICE);
+        }
         startForeground(101, builder.build());
 
         // 闹钟也会发送这个广播
